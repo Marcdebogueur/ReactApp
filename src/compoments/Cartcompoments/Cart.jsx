1ed65be1e16f43940656db85cf62f98046e4c025
Marcdebogueur <marckouyate1@gmail.com>
2024-02-23 15:30:12 +0100

    gestion du panier

---
+++ src/compoments/Cartcompoments/Cart.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+import "../Style/Cart.css"
+
+const Cart = ({cart, setCart, handleChange}) => {
+
+  const [price, setPrice]=useState(0)
+
+  const handleremove = (id) => {
+    const arr = cart.filter((item) => item.id !== id);
+    setCart(arr);
+    handlePrice();
+  };
+
+  const handlePrice = () => {
+    let ans = 0;
+    cart.map((item) => (ans += item.amount * item.price));
+    setPrice(ans);
+  };
+  useEffect(() => {
+    handlePrice();
+  });
+  return(
+  <div>
+  <article>
+    {cart?.map((item) => (
+      <div className="cart_box" key={item.id}>
+        <div className="cart_img">
+          <img src={item.img} alt="img" />
+          <p>
+            {item.title} <span className="item_price">${item.price}</span>
+          </p>
+        </div>
+        <div className="add_subtractbtn">
+          <button onClick={() => handleChange(item, +1)}> + </button>
+          <button> {item.amount}</button>
+          <button onClick={() => handleChange(item, -1)}> - </button>
+        </div>
+        <div className="price_btn">
+          <button onClick={() => handleremove(item.id)}>Remove</button>
+        </div>
+      </div>
+    ))}
+    <div className="total">
+      <span>Total Price of your Cart</span>
+      <span>=$ {price}</span>
+    </div>
+  </article>
+</div>
+);
+}
+
+export default Cart
--- src/compoments/Cartform.js
+++ src/compoments/Cartform.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Navbar from './Cartcompoments/Navbar'
 import Amazon from './Cartcompoments/Amazon'
 import Shopcart from './Cartcompoments/Shopcart'
+import Cart from './Cartcompoments/Cart'
 
 const Cartform = () => {
     const [show, setShow]=useState(true)
@@ -9,19 +10,35 @@ const Cartform = () => {
 
     const handleClick=(item)=>{
       // console.log(item)
-      if(!cart.includes(item)){
-        cart.push(item)
-        console.log(cart)
-      }else
-      console.log(cart)
-     
-    }
+    //   if(!cart.includes(item)){
+    //     cart.push(item)
+    //     console.log(cart)
+    //   }else
+    //   console.log(cart)
+    if(cart.indexOf(item)!==-1) return;
+    setCart([...cart, item])  
+    };
+    const handleChange = (item, d)=>{
+        const ind =cart.indexOf(item);
+        const arr =cart
+         console.log(ind, arr)
+        arr[ind].amount =+d;
+
+        if(arr[ind].amount === 0){
+            arr[ind].amount =1
+        }else{
+            setCart([...arr]);
+        }
+    };
+
+
   return (
     <div>
-        <Navbar setShow={setShow}/>
+        <Navbar setShow={setShow} size={cart.length}/>
         {/* <Amazon/> */}
-        {show ? <Amazon handleClick={handleClick}/> : "cart"}
-        <Shopcart/>
+        {show ? (<Amazon handleClick={handleClick}/> )
+        :( <Cart cart={cart} setCart={setCart} handleChange={handleChange} />)}
+        
     </div>
   )
 }
